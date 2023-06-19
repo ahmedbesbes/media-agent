70440ee438af4d6d5786ff5a57357876efe8efe2
Ahmed Besbes <ahmed.besbes@hotmail.com>
2023-06-20 00:56:26 +0200

    Update prompt to avoid having questions with no answers

--- src/twitter/utils/prompts.py
+++ src/twitter/utils/prompts.py
@@ -3,6 +3,7 @@ summarization_template = """Given the following tweets
 {text}
 
 I want you to provide a short summary and produce three questions that cover the discussed topics.
+Each question should find its answer within the tweets. Don't invent questions that have no answers.
 
 Format the output as a JSON with the following keys and do not forget the curly brackets.
 
