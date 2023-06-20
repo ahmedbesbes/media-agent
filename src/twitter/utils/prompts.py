2accb10ed6e0acb1e39726b9224a8d9be7e5150a
Ahmed Besbes <ahmed.besbes@hotmail.com>
2023-06-20 13:38:05 +0200

    Update summarization template

--- src/twitter/utils/prompts.py
+++ src/twitter/utils/prompts.py
@@ -4,6 +4,7 @@ summarization_template = """Given the following tweets
 
 I want you to provide a short summary and produce three questions that cover the discussed topics.
 Each question should find its answer within the tweets. Don't invent questions that have no answers.
+Questions should also be very different from each other.
 
 Format the output as a JSON with the following keys and do not forget the curly brackets.
 
