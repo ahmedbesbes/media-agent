9ea98daf808acc992a680cdf6aea64d2085b94f5
Ahmed Besbes <ahmed.besbes@hotmail.com>
2023-06-23 10:22:12 +0100

    Add history and adjust path

--- src/utils/twitter_agent.py
+++ src/utils/twitter_agent.py
@@ -41,7 +41,7 @@ class TwitterAgent(object):
             self.console.log(
                 "Saving conversation history with sources and metadata ..."
             )
-            with open("history.json", "w") as f:
+            with open("data/history.json", "w") as f:
                 json.dump(self.history, f)
 
         atexit.register(save_history)
